e1704664d248b07a0f2367960ea07894cd9a80d4
eugenekhoo <ktw1995@gmail.com>
2023-08-04 16:00:08 +0800

    updated cors options

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5000",
   "http://localhost:3000",
-  "https://react-twitter-clone-gilt.vercel.app/",
+  "https://react-twitter-clone-gilt.vercel.app",
 ];
 
 module.exports = allowedOrigins;
