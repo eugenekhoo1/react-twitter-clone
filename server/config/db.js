10e6bc0b5a6207854fd6191804c05b77e7ad25d8
eugenekhoo <ktw1995@gmail.com>
2023-08-07 09:20:19 +0800

    updated db settings

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,16 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOSTNAME,
-  port: process.env.DB_PORT,
-  database: process.env.DB,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.INTERNAL_DB_URL,
 });
+
+// const pool = new Pool({
+//   user: process.env.DB_USERNAME,
+//   host: process.env.DB_HOSTNAME,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB,
+//   password: process.env.DB_PASSWORD,
+// });
 
 // const pool = new Pool({
 //   user: "postgres",
