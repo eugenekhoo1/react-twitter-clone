1591fe13604c797b5e521c0df0db0a328de34436
eugenekhoo <ktw1995@gmail.com>
2023-08-07 09:34:44 +0800

    updated db settings

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.INTERNAL_DB_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 // const pool = new Pool({
