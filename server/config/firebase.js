ab83714e4c75de121234a69f3a6a9385e1012596
eugenekhoo <ktw1995@gmail.com>
2023-09-04 19:47:09 +0800

    update env variables

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");
 const { getStorage } = require("firebase/storage");
+require("dotenv").config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCJmpbLhyasKWjCWY7YLLWySbkp2e455_8",
-  authDomain: "tweeter-image-db.firebaseapp.com",
-  projectId: "tweeter-image-db",
-  storageBucket: "tweeter-image-db.appspot.com",
-  messagingSenderId: "564124049821",
-  appId: "1:564124049821:web:86ec2db6ff5fd8b0678f74",
-  measurementId: "G-WY2JXXZ3EQ",
+  apiKey: process.env.FB_API,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
